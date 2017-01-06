45c3a86b5fb44922d9c5b825761accb7bd109055
Krystian <krystianfranus@users.noreply.github.com>
2017-01-06 14:36:39 +0100

    shit

--- api/index.php
+++ api/index.php
@@ -29,6 +29,24 @@ if($uri === $path.'/films') {
 		$status = new JSendResponse('fail', array("message"=>"Błąd bazy danych"));
 	echo json_encode($status);
 
+}elseif($uri === $path.'/countries') {
+	$result = $obj->listAllCountries();
+	$tab = pg_fetch_all($result);
+	if($tab){
+		$status = new JSendResponse('success', $tab);
+	}else 
+		$status = new JSendResponse('fail', array("message"=>"Błąd bazy danych przy wyświetlaniu wszystkich krajów"));
+	echo json_encode($status);
+
+}elseif($uri === $path.'/genres') {
+	$result = $obj->listAllGenres();
+	$tab = pg_fetch_all($result);
+	if($tab){
+		$status = new JSendResponse('success', $tab);
+	}else 
+		$status = new JSendResponse('fail', array("message"=>"Błąd bazy danych przy wyświetlaniu wszystkich gatunków"));
+	echo json_encode($status);
+
 }elseif(preg_match("/^".$pathreg."\/films\/[0-9]{1,}$/", $uri, $match)) {
 	preg_match('!\d+!', $uri, $matches);
 	$result = $obj->listMovie($matches[0]);
@@ -57,6 +75,23 @@ if($uri === $path.'/films') {
 	}
 	else{
 		$status = new JSendResponse('fail', array("message"=>"Nie dodano filmu"));		
+	}
+	echo json_encode($status);
+}elseif($uri === $path.'/countries/add'){
+	//$data = json_decode($_POST, true);
+	//echo $_POST["tytul"];
+	//print_r($_POST);
+
+	$response = $obj->addCountry($_POST);
+
+	if($response == 1){ // za $_POST ma isc $data
+		$status = new JSendResponse('success', array("message"=>"Kraj dodano poprawnie"));
+	}
+	elseif($response == 0){
+		$status = new JSendResponse('error', array("message"=>"Taki kraj już istnieje"), 'Not cool.', 9001);		
+	}
+	else{
+		$status = new JSendResponse('fail', array("message"=>"Nie dodano kraju"));		
 	}
 	echo json_encode($status);
 }elseif(preg_match("/^".$pathreg."\/films\/remove\/[0-9]{1,}$/", $uri, $match)){
